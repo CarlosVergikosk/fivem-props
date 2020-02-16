9dd72bfb08e62be91e7c6729679d407cec3840d1
CarlosVergikosk <45525595+CarlosVergikosk@users.noreply.github.com>
2020-02-16 15:14:25 +0000

    First Commit

---
+++ b1g_props/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+name 'B1G'
+author 'B1G - https://github.com/CarlosVergikosk'
+version 'v1.0'
+
+client_script {
+	'prop.lua'
+}
+dependencies {
+	'es_extended'
+}
+
+exports {
+	'attach',
+	'removeall'
+}
+
+
